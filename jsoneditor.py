242ed65a2597693da84d39f737d357e8fc548215
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2021-04-08 20:29:17 -0600

    beeg update
    added a "saving" system for last dir used, kinda buggy but its almost there

--- jsoneditor.py
+++ jsoneditor.py
@@ -1,8 +1,12 @@
 #!usr/bin/python3
 import json
 import PySimpleGUI as sg
+import os
 
-print(f"Using PySimpleGUI Version: {sg.version}")
+#print(f"Using PySimpleGUI Version: {sg.version}")
+
+openLastDir = open("lastdir.json", "r")
+lastUsedDir = json.load(openLastDir)
 
 sg.theme('DarkGrey4')   # Add a little color to your windows
 # All the stuff inside your window. This is the PSG magic code compactor...
@@ -10,29 +14,65 @@ layout = [  [sg.Text('Parent Model'), sg.InputText("block/cube_all")],
             [sg.Text('Block/Item'), sg.InputText("block")],
             [sg.Text('Name'), sg.InputText("birch_planks")],
             [sg.Text('ModID'), sg.InputText("minecraft")],
-            [sg.Button("Save"), sg.Button("Close")]]
+            [sg.Checkbox('Auto Save to last used dir'), sg.Button('!')],
+            [sg.Button("Save"), sg.Button("Close")]
+            ]
 
 # Create the Window
 window = sg.Window('Minecraft Model Generator', layout)
 # Event Loop to process "events"
 while True:             
     event, values = window.read()
-    if event in (sg.WIN_CLOSED, 'Close'):
+
+    if (event == "!"):
+        sg.popup_non_blocking("Only use after you have chosen to save a file, it will save to the current folder otherwise! \nNOTE: Not persistant throughout sessions.")
+
+    if (event == sg.WINDOW_CLOSE_ATTEMPTED_EVENT or event == 'Close') and sg.popup_yes_no('Do you really want to exit?') == 'Yes':
+        openLastDir.close()
         break
-    if event in ("Save"):
-        saveLocation = sg.popup_get_folder(f"Folder to save {values[2]}.json To", title="Save Location", default_path="", no_window=True)
-        with open(f"{saveLocation}/{values[2]}.json", "w") as JsonOpen:
-            JsonLayout = {
-                    "parent": f"{values[3]}:" f"{values[0]}",
-                    "textures": {
-                        "all": f"{values[3]}:" f"{values[1]}/{values[2]}"
+    if event == "Save":
+        #If the checkbox to save directly to last used folder without the popup, is not checked, give the popup, otherwise dont.
+        if values[4] == False:
+            #This is really buggy because it will pull whatever was saved *last* time, not every time you open the menu, even if the json has been updated
+            #im not sure how to update it everytime, probably going to be something in the while True loop here or something
+            #its gonna be a headache though
+            saveLocation = sg.popup_get_folder(f"Folder to save {values[2]}.json To", title="Save Location", default_path=lastUsedDir["lastFolder"])
+        elif values[4] == True:
+            saveLocation = lastUsedDir["lastFolder"]
+
+        #Cancel button returns "None", so if there is anything but "None", save the File
+        if saveLocation != None:
+            with open(f"{saveLocation}/{values[2]}.json", "w") as JsonOpen:
+                JsonLayout = {
+                        "parent": f"{values[3]}:" f"{values[0]}",
+                        "textures": {
+                            "all": f"{values[3]}:" f"{values[1]}/{values[2]}"
+                        }
                     }
-                
-            }
-            
-            JsonDump = json.dumps(JsonLayout, indent=4)
-            JsonOpen.write(JsonDump)
-        
-        sg.popup(f"saved {values[2]}.json")
+                JsonDump = json.dumps(JsonLayout, indent=4)
+                JsonOpen.write(JsonDump)
+            sg.popup(f"saved {values[2]}.json")
+            print("saved file")
+
+            #Save last used directory to a json, so it auto completes (if the option is enabled)
+            #Also really hacky because idk wtf im doing
+            with open("lastdir.json", "r") as readDir:
+                lastFolder = json.load(readDir)
+                if lastFolder["lastFolder"] == saveLocation:
+                    sameFolder = True
+                else:
+                    sameFolder = False
+            if sameFolder == False:
+                with open("lastdir.json", "w") as writeDir:
+                    yep = {
+                        "lastFolder":  f"{saveLocation}"
+                    }
+                    nope = json.dumps(yep)
+                    writeDir.write(nope)
+                    print("saved last used directory")
+            else:
+                continue
+        else:
+            print("Canceling Saving")
 
 window.close()
